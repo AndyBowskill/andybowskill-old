48454f976502c539248b2a69c0b356a1d775eaf4
Andy Bowskill <andy@andybowskill.co.uk>
2013-04-10 23:10:06 +0100

    Redesigned page nav and hero unit

--- assets/js/application.js
+++ assets/js/application.js
@@ -9,29 +9,4 @@ $(function() {
 		});
 	});
 
-	$('section[id=iOS]').show(function() {
-
-		//Fix the subnav element to top if the original position scrolls out of view
-		var $win = $(window)
-		  , $nav = $('.subnav')
-		  , navTop = $('.subnav').length &&$('.subnav').offset().top - 36
-		  , isFixed = 0;
-
-		processScroll()
-		$win.on('scroll', processScroll)
-
-		function processScroll() {
-		  var i, scrollTop = $win.scrollTop();
-		  if (scrollTop >= navTop && !isFixed) {
-		    isFixed = 1;
-		    $nav.addClass('subnav-fixed');
-		  } else if (scrollTop <= navTop && isFixed) {
-		    isFixed = 0;
-		    $nav.removeClass('subnav-fixed');
-		  }
-		}
-	});
-
 });
-
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
